38ea52225aa79d5e41e42584037037048a375098
Patrick Schwederski <paddy@alli-go.de>
2025-05-30 11:26:16 +0200

    improved button block to handle more functionality

--- src/Block/ButtonBlock/ButtonBlock.cpp
+++ src/Block/ButtonBlock/ButtonBlock.cpp
@@ -1,27 +1,63 @@
 #include "ButtonBlock.h"
 
+ButtonBlockParameter DEFAULT_BUTTON_PARAMETERS = ButtonBlockParameter{
+    .shortPressLimit = 1000UL,
+    .longPressMinimum = 2000UL,
+    .timeout = 10000UL
+};
+
 ButtonBlock::ButtonBlock(ButtonBlockConfiguration* blockConfiguration) {
     this->genericBlockConfiguration = blockConfiguration;
     this->blockConfiguration = blockConfiguration;
     this->blockConfiguration->parentBlock = this; // Set the parent block reference
+    this->currentState = ButtonState::NO_PRESS;
+    this->timestamp = 0;
 }
 
 ButtonBlock::~ButtonBlock() {
     // Destructor implementation
 }
 
-block_err_t ButtonBlock::executeBlock() {
-    uint8_t buttonState = digitalRead(PIN_BUTTON_PRESS);
-    if(buttonState == LOW) { // Assuming LOW means pressed
-            this->wasPressed = true;
-    } 
+block_err_t ButtonBlock::enterBlock() {
+    this->reset();
     return 0;
 }
 
-bool ButtonBlock::wasTriggered() {
-    return this->wasPressed;
+block_err_t ButtonBlock::executeBlock() {
+    uint8_t buttonState = digitalRead(PIN_BUTTON_PRESS);
+    uint64_t currentTimestamp = millis();
+
+    if(buttonState == LOW && this->currentState == ButtonState::NO_PRESS) { // Assuming LOW means pressed
+        ESP_LOGI("BUTTON", "Button pressed");    
+        this->currentState = ButtonState::PRESSED;
+        this->timestamp = currentTimestamp;
+    } 
+
+
+    if(buttonState == HIGH && this->currentState == ButtonState::PRESSED) {
+        //button was released. Check if it was a short press or a long press
+        if(this->timestamp + this->blockConfiguration->parameters->shortPressLimit >= currentTimestamp) {
+            ESP_LOGI("BUTTON", "Short press detected");
+            this->currentState = ButtonState::SHORT_PRESS;
+        } else if(this->timestamp + this->blockConfiguration->parameters->longPressMinimum <= currentTimestamp) {
+            ESP_LOGI("BUTTON", "Long press detected");
+            this->currentState = ButtonState::LONG_PRESS;
+        }
+    }
+
+    if(this->blockConfiguration->nextBlockNameTimeout != nullptr && buttonState == HIGH && (this->timestamp + this->blockConfiguration->parameters->timeout < currentTimestamp)){
+            ESP_LOGI("BUTTON", "Timeout detected");
+            this->currentState = ButtonState::TIMEOUT;
+        }
+
+    return 0;
+}
+
+ButtonState ButtonBlock::getState() {
+    return this->currentState;
 }
 
 void ButtonBlock::reset() {
-    this->wasPressed = false;
+    this->currentState = ButtonState::NO_PRESS;
+    this->timestamp = 0;
 }
--- src/Block/ButtonBlock/ButtonBlock.h
+++ src/Block/ButtonBlock/ButtonBlock.h
@@ -9,38 +9,111 @@
 
 class ButtonBlockConfiguration;
 
+enum class ButtonState {
+    NO_PRESS,
+    PRESSED,
+    SHORT_PRESS,
+    LONG_PRESS,
+    TIMEOUT
+};
 
 class ButtonBlock : public VBlock {
 
     protected:
     ButtonBlockConfiguration* blockConfiguration;
-    bool wasPressed = false;
+    ButtonState currentState = ButtonState::NO_PRESS;
+    uint64_t timestamp = 0;
 
 public:
     ButtonBlock(ButtonBlockConfiguration* blockConfiguration);
     ~ButtonBlock();
 
     block_err_t executeBlock() override;
-    bool wasTriggered();
+    ButtonState getState();
     void reset();
+    block_err_t enterBlock() override;
 };
 
+struct ButtonBlockParameter {
+    uint64_t shortPressLimit;
+    uint64_t longPressMinimum;
+    uint64_t timeout;
+} ;
+
+extern ButtonBlockParameter DEFAULT_BUTTON_PARAMETERS;
 
 struct ButtonBlockConfiguration : public IBlockConfiguration {  
 
     ButtonBlock* parentBlock;
-    ButtonBlockConfiguration(const char* blockName, const char* nextBlockName)
-        : IBlockConfiguration(blockName, BlockType::WAIT_FOR_PRESS, nextBlockName) {
+    const char* nextBlockNameShortPress = nullptr;
+    const char* nextBlockNameLongPress = nullptr;
+    const char* nextBlockNamePressed = nullptr;
+    const char* nextBlockNameTimeout = nullptr;
+
+    ButtonBlockParameter* parameters = &DEFAULT_BUTTON_PARAMETERS;
+
+    ButtonBlockConfiguration(
+            const char* blockName, 
+            const char* nextBlockNameAnyPress , 
+            const char* nextBlockNameShortPress = nullptr, 
+            const char* nextBlockNameLongPress = nullptr, 
+            const char* nextBlockNamePressed = nullptr,
+            const char* nextBlockNameTimeout = nullptr,
+            ButtonBlockParameter* parameters = nullptr    
+        )
+    
+        : IBlockConfiguration(blockName, BlockType::WAIT_FOR_PRESS, nextBlockNameAnyPress) {
+        
+            this->nextBlockNamePressed = nextBlockNamePressed;
+            this->nextBlockNameShortPress = nextBlockNameShortPress;
+            this->nextBlockNameLongPress = nextBlockNameLongPress;
+            this->nextBlockNameTimeout = nextBlockNameTimeout;
+
+            if(parameters == nullptr) {
+                this->parameters = &DEFAULT_BUTTON_PARAMETERS;
+            } else {
+                this->parameters = parameters;
+            }
+        
         }
 
     const char* getNextBlockName() override {
-        if(this->parentBlock->wasTriggered()) {
-            this->parentBlock->reset();
-            return this->nextBlockName;
-        } else {
-            ESP_LOGD("BUTTONBLOCK", "Button not pressed, returning current block name");
-            return this->blockName;
+        switch(this->parentBlock->getState()) {
+            case ButtonState::NO_PRESS:
+                return this->blockName;
+                break;
+            case ButtonState::SHORT_PRESS:
+                if(this->nextBlockNameShortPress != nullptr) {
+                    this->parentBlock->reset();
+                    return this->nextBlockNameShortPress;
+                }
+                break;
+            case ButtonState::LONG_PRESS:
+                if(this->nextBlockNameLongPress != nullptr) {
+                    this->parentBlock->reset();
+                    return this->nextBlockNameLongPress;
+                }
+                break;
+            case ButtonState::PRESSED:
+                if(this->nextBlockNamePressed != nullptr) {
+                    return this->nextBlockNamePressed;
+                }
+                break;
+            case ButtonState::TIMEOUT:
+                if(this->nextBlockNameTimeout != nullptr) {
+                    this->parentBlock->reset();
+                    return this->nextBlockNameTimeout;
+                }
+                break;
         }
+
+        //check if button was pressed and run the generic handle
+        if(this->nextBlockName != nullptr && (this->parentBlock->getState() == ButtonState::LONG_PRESS || this->parentBlock->getState() == ButtonState::SHORT_PRESS)) {
+            return this->nextBlockName;
+        }
+
+        //if no item was triggered, fallback to repeat the block
+        return this->blockName;
     }
 };
 
